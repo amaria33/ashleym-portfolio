c77ae2a767755f3159e38ae1e6229c4b2e1bb7cd
amaria33 <ashleymaria@MacBookPro.lan>
2025-12-04 23:05:46 -0600

    Fix import path for CurrentYear component in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import CurrentYear from "../../components/CurrentYear";
+import CurrentYear from "../components/CurrentYear";
 
 export default function Home() {
   return (
